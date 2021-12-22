2d6958b968d775f1dfca940549faeace4a0322ae
Tabot Chares Bessong <ebezebeatrice@gmail.com>
2021-12-22 19:58:00 +0100

    started working on socket.io

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+
+
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http');
+const PORT  = process.env.PORT || 5000;
+
+const router = require('./router')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection',(socket)=>{
+  console.log('we have a new connection');
+  socket.on('join',({name,room},callback)=>{
+    console.log(name,room);
+
+    const error  = true 
+
+    if(error){
+      callback({error:'error'})
+    }    
+  })
+  socket.on('disconnect',()=>{
+    console.log('user just left');
+  })
+})
+
+app.use(router)
+
+server.listen(PORT,()=>{console.log(`server is running on port ${PORT} `);}) 
+
--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './index.css'
+import queryString from 'query-string';
+import io from 'socket.io-client'
 
-const Chat = () => {
+let socket 
+
+const Chat = ({location}) => {
+  const [name,setName] = useState('')
+  const [room,setRoom] = useState('')
+  const endPoint = 'localhost:5000'
+
+  useEffect(()=>{
+    const {room,name} = queryString.parse(location.search)
+
+    socket = io(endPoint)
+    // console.log(name,room);
+    setName(name)
+    setRoom(room)
+
+    socket.emit('join',{name,room},({error})=>{
+      alert(error)
+    })
+
+    console.log(socket);
+  },[endPoint,location.search])
   return (
     <>
-      <h2>lets chat</h2>
+
     </>
   )
 }
