ae93bd26580bd64e10668b3dd71bd9abbc3b092e
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-04-15 19:05:52 +0100

    innertialised portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis tempore suscipit quae at voluptatum sequi aliquam, commodi enim nam rerum consequuntur nemo ex assumenda sunt perferendis. Quisquam rem cum officia eius sunt quod id fugit reprehenderit consectetur, nihil, facilis quae nemo. Autem porro velit maxime earum minima quae sed nulla.</h2>
+    </div>
+  )
+}
+
+export default App
