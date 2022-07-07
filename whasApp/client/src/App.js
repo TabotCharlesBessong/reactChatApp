0db8d9f1a170739b538e1acb20d42627349365c2
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-07-07 07:49:35 +0100

    lets get started

---
+++ whasApp/client/src/App.js
@@ -0,0 +1,12 @@
+import React,{useState,useEffect} from 'react'
+import './App.scss'
+
+const App = () => {
+  return (
+    <div className='app' >
+      <h2>Hello world</h2>
+    </div>
+  )
+}
+
+export default App
