0adabb9447c6b00febe90b4633911e278b85e255
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-10-06 06:56:16 +0200

    server running

--- roadSideCoder/server/index.js
+++ roadSideCoder/server/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+
+const express = require('express')
+
+
+const app = express()
+const port = 5000 || process.env.PORT 
+
+app.get('/',(req,res)=>{
+  res.send('<h1>Hello from homepage</h1>')
+})
+
+app.get('')
+
+app.listen(port,()=>{
+  console.log(`Our server has started on server number ${port} `)
+})
