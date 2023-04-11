c1afffac9d358a4192fc6dff855a4861a2570288
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-04-11 15:16:38 +0100

    getting rid of what is not ment to be

--- portfolio/src/container/index.js
+++
@@ -1,7 +0,0 @@
-
-export {default as About  } from './about/About'
-export {default as Work  } from './work/Work'
-export {default as Header  } from './header/Header'
-export {default as Footer  } from './footer/Footer'
-export {default as Skills  } from './skills/Skills'
-export {default as Testimonials  } from './testimonials/Testimonials'
