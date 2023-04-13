b0a34b85e4f0fd3c6456aaa41298dfe5ab50aaa5
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-04-13 13:06:44 +0100

    typing event

--- chatAppLearning/client/src/component/ChatWindow.jsx
+++ chatAppLearning/client/src/component/ChatWindow.jsx
@@ -5,6 +5,7 @@ import {
   Card,
   IconButton,
   InputAdornment,
+  InputLabel,
   OutlinedInput,
   Typography,
 } from "@mui/material";
@@ -15,6 +16,8 @@ const ChatWindow = () => {
   const [socket, setSocket] = useState(io("http://localhost:5000"));
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+  const [typing, setTyping] = useState(false);
+  const [typingTimeout, setTypingTimeout] = useState(null);
 
   useEffect(() => {
     // setSocket(io("http://localhost:5000"));
@@ -25,7 +28,12 @@ const ChatWindow = () => {
     socket.on("message-from-server", (data) => {
       // console.log('message received from server',data)
       setChat((prev) => [...prev, {message:data.message,received:true}]);
-      console.log({ chat });
+    });
+    socket.on("typing-started-from-server", () => {
+      setTyping(true)
+    });
+    socket.on("typing-stoped-from-server", () => {
+      setTyping(false)
     });
   }, [socket]);
 
@@ -36,6 +44,15 @@ const ChatWindow = () => {
     setChat((prev) => [...prev, {message,received:false}]);
     setMessage("");
   };
+
+  const handleInput = (e) => {
+    setMessage(e.target.value);
+    socket.emit('typing-started')
+    if(typingTimeout) clearTimeout(typingTimeout);
+    setTypingTimeout(setTimeout(() => {
+      socket.emit('typing-stoped')
+    },1000))
+  }
   return (
     <Box xs={{display:'flex',justifyContent:'center'}} >
       <Card sx={{padding:2,marginTop:10,width:'60%',backgroundColor:'orangered'}} >
@@ -47,11 +64,19 @@ const ChatWindow = () => {
           ))}
         </Box>
         <Box component="form" onSubmit={handleForm}>
+          {
+            typing && (
+              <InputLabel sx={{color:'white'}} shrink htmlFor='message-input' >
+                typing...
+              </InputLabel>
+            )
+          }
           <OutlinedInput
             type="text"
             sx={{backgroundColor:'white',width:'100%'}}
             value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            onChange={handleInput}
+            id='message-input'
             placeholder="write your message"
             endAdornment={
               <InputAdornment position="end">
--- chatAppLearning/server/index.js
+++ chatAppLearning/server/index.js
@@ -29,7 +29,12 @@ io.on('connection',(socket) => {
   socket.on('send-message',(data) => {
     socket.broadcast.emit('message-from-server',data)
     console.log('Message received')
-    console.log(data)
+  })
+  socket.on('typing-started',() => {
+    socket.broadcast.emit('typing-started-from-server')
+  })
+  socket.on('typing-stoped',() => {
+    socket.broadcast.emit('typing-stoped-from-server')
   })
   socket.on('disconnect',(socket) => {
     console.log('User left the chat')
